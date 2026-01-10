1cabc92074e691064bfabe3f7929b9fbbe509b3f
Salim kök <155094272+SalimKok@users.noreply.github.com>
2026-01-11 02:03:36 +0300

    firebase başlatma kodu eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:roomai/firebase_options.dart';
 import 'package:roomai/screens/landing_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: MyApp()));
 }
 
